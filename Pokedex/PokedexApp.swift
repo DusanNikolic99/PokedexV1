336a3aa25b860dac73010984842cc223620d7272
Dusan Nikolic <Dusan.Nikolic@student.nhh.no>
2026-03-02 22:31:04 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Dusan Nikolic on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
